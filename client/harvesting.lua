5b2bcb024230d0943e02ae9e586f2ee11b18e58d
Scott Lee <scottlee5455@outlook.com>
2025-04-14 16:05:40 -0500

    setup of harvesting.lua

---
+++ client/harvesting.lua
@@ -0,0 +1,27 @@
+-- Handle harvesting interactions
+
+for drug, data in pairs(Config.Zones.Harvest) do 
+    exports['qb-target']:AddBoxZone(drug .. 'harvest', data.coords, data.size.x, data.size.y, {
+        name = drug .. 'harvest',
+        heading = 0,
+        debugPoly = false,
+        minZ = data.coords.z - 1,
+        maxZ = data.coords.z + 1,
+    }, {
+        options = {
+            {
+                event = 'rwhdrugscript:harvest',
+                icon = 'fas fa-leaf',
+                label = 'Harvest ' .. drug,
+                drug = drug,
+                action = function()
+                    TriggerServerEvent('rwhdrugscript:harvest', drug)
+                end,
+                canInteract = function()
+                    return not IsPedInAnyVehicle(PlayerPedId(), false)
+                end,
+            },
+        },
+        distance = 2.5,
+    })
+end
